3a5f97f20048bd8fa9b7023845505980dcb81dca
AleksandrF <a7214370@gmail.com>
2022-03-29 14:39:13 +0300

    App completed

---
+++ NotesApp/ViewControllers/NoteViewController.swift
@@ -0,0 +1,23 @@
+//
+//  NoteViewController.swift
+//  NotesApp
+//
+//  Created by Aleksandr F. on 29.03.2022.
+//
+
+import UIKit
+
+class NoteViewController: UIViewController {
+    
+    @IBOutlet var titleLabel: UILabel!
+    @IBOutlet var noteLabel: UITextView!
+    
+    public var noteTitle: String = ""
+    public var note: String = ""
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        titleLabel.text = noteTitle
+        noteLabel.text = note
+    }
+}
